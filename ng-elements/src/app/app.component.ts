d1c1e06e03a940bafa8d9dcb685991fc1980b46e
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 09:49:56 +0100

    ng-elelments - implementation and usage of contact card

--- ng-elements/src/app/app.component.ts
+++ ng-elements/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-elements';
+
+  popup(data) {
+    alert(JSON.stringify(data));
+  }
 }
--- ng-elements/src/app/contact-card/contact-card.component.ts
+++ ng-elements/src/app/contact-card/contact-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-contact-card',
   template: `
-    <p>
-      contact-card works!
-    </p>
+    <div>
+        hello {{name}}
+        <input [(ngModel)]="phone" />
+        <button (click)="more.emit({name:name, phone:phone})">more</button>
+    </div>
   `,
   styles: []
 })
 export class ContactCardComponent implements OnInit {
+
+  @Input() name;
+  @Input() phone;
+  @Output() more = new EventEmitter();
 
   constructor() { }
 
