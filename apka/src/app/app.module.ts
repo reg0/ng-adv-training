767bdc135a5f85933bd88b49ddee6be6b6ed1b8a
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 15:48:59 +0100

    apka - added mat card wrapper

--- apka/src/app/app.module.ts
+++ apka/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -28,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
     HttpClientModule
   ],
   providers: [],
