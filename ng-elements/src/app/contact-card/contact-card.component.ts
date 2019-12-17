4252ac0fb3e0388b0c5074d74264c9f6098851cd
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 09:37:19 +0100

    ng-elements - added empty component

---
+++ ng-elements/src/app/contact-card/contact-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-card',
+  template: `
+    <p>
+      contact-card works!
+    </p>
+  `,
+  styles: []
+})
+export class ContactCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
