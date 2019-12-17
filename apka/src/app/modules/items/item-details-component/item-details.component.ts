ac4c1a6c8a91ec62160df23177e6c3e5d43c9d42
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 16:08:29 +0100

    apka - displaying item details

--- apka/src/app/modules/items/item-details-component/item-details.component.ts
+++ apka/src/app/modules/items/item-details-component/item-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemsService } from '../items.service';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-item-details',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-details.component.scss']
 })
 export class ItemDetailsComponent implements OnInit {
+  data$: Observable<any>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private itemsService: ItemsService) { }
 
   ngOnInit() {
+    this.data$ = this.route.params.pipe(
+      switchMap(value => this.itemsService.get(value.id)),
+    );
   }
-
 }
