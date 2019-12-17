ae9a1473155e06243040a1301122726b63bd2b57
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 13:39:34 +0100

    apka - ng-template, ng-container and ngTemplateOutlet data communication

--- ng-shared/src/app/shared/components/data-grid/data-grid.component.ts
+++ ng-shared/src/app/shared/components/data-grid/data-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-data-grid',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-grid.component.scss']
 })
 export class DataGridComponent implements OnInit {
+
+  @ContentChild(TemplateRef, {static: true})
+  contentsTpl: TemplateRef<any>;
 
   constructor() { }
 
