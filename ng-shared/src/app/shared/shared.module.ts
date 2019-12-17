de3238f19bea95ce5ed7966159652ac548896893
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 13:14:22 +0100

    apka - added shared module and empty data-grid component in ng-shared

---
+++ ng-shared/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataGridComponent } from './components/data-grid/data-grid.component';
+
+
+
+@NgModule({
+  declarations: [DataGridComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
