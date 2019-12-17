5321e1ac5e60d745efef9f228c74d1dd4e21acb6
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 10:09:59 +0100

    ng-elements - declaring custom component angular way

--- ng-elements/src/app/app.module.ts
+++ ng-elements/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactCardComponent } from './contact-card/contact-card.component';
 import { FormsModule } from "@angular/forms";
-
+import { createCustomElement } from "@angular/elements";
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,16 @@ import { FormsModule } from "@angular/forms";
     AppRoutingModule,
     FormsModule,
   ],
+  entryComponents: [ContactCardComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {
+    const el = createCustomElement(ContactCardComponent, {
+      injector: this.injector
+    });
+
+    customElements.define('y-contact-card', el);
+  }
+}
