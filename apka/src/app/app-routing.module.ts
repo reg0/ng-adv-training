f70c7c4db1bfc4592ecaae478c1d47565cf613ef
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-17 11:28:03 +0100

    apka - added routing with lazy loading

--- apka/src/app/app-routing.module.ts
+++ apka/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cart', loadChildren: () => import ('./modules/cart/cart.module').then(m => m.CartModule)},
+  {path: 'items', loadChildren: () => import ('./modules/items/items.module').then(m => m.ItemsModule)},
+  // {path: 'cart', loadChildren: () => import ('./modules/cart/cart.module').then(m => m.CartModule)},
+  // {path: 'cart', loadChildren: () => import ('./modules/cart/cart.module').then(m => m.CartModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
