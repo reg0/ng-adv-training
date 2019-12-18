0d538eaac958ad5914ef72a9694a321debb86a8f
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 10:43:58 +0100

    apka / ngrx - addToCart handling in reducer using entity adapter

--- apka/src/app/modules/cart/store/cart.reducer.ts
+++ apka/src/app/modules/cart/store/cart.reducer.ts
@@ -1,15 +1,20 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import * as CartActions from './cart.actions';
-
+import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
+import { CartItemModel } from 'src/app/utils/models';
 export const cartFeatureKey = 'cart';
 
-export interface State {
+export const cartAdapter: EntityAdapter<CartItemModel> = createEntityAdapter<CartItemModel>();
 
+export interface State extends EntityState<CartItemModel> {
+  loading: boolean;
+  loaded: boolean;
 }
 
-export const initialState: State = {
-
-};
+export const initialState: State = cartAdapter.getInitialState({
+  loading: false,
+  loaded: false,
+});
 
 const cartReducer = createReducer(
   initialState,
@@ -18,7 +23,7 @@ const cartReducer = createReducer(
   on(CartActions.loadCartsSuccess, (state, action) => state),
   on(CartActions.loadCartsFailure, (state, action) => state),
   on(CartActions.addToCart, (state, {data}) => {
-    return {...state, data};
+    return cartAdapter.upsertOne(data, {...state, loading: true});
   }),
 
 );
