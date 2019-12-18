91016fc28cbe40fa08a3429049d273d97f324238
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 14:11:28 +0100

    forms - added form-generator in ng-shared

--- ng-shared/src/app/shared/shared.module.ts
+++ ng-shared/src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataGridComponent } from './components/data-grid/data-grid.component';
+import { FormGeneratorComponent } from '../components/form-generator/form-generator.component';
 
 
 
 @NgModule({
-  declarations: [DataGridComponent],
+  declarations: [DataGridComponent, FormGeneratorComponent],
   imports: [
     CommonModule
   ],
   exports: [
     DataGridComponent,
+    FormGeneratorComponent,
   ],
 })
 export class SharedModule { }
