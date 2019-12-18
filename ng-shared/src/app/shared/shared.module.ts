329088152f90f23c1c1c456f4ef4b9e103364282
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 14:25:37 +0100

    forms - added ReactiveFormsModule to ng-shared

--- ng-shared/src/app/shared/shared.module.ts
+++ ng-shared/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataGridComponent } from './components/data-grid/data-grid.component';
 import { FormGeneratorComponent } from '../components/form-generator/form-generator.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [DataGridComponent, FormGeneratorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [
     DataGridComponent,
