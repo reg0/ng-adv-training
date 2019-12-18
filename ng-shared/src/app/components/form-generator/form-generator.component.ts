ab97858982ad2267871d84caabb0972d85276e01
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 14:37:13 +0100

    forms - parsing config, passing to form builder

--- ng-shared/src/app/components/form-generator/form-generator.component.ts
+++ ng-shared/src/app/components/form-generator/form-generator.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { FieldConfig } from 'src/app/utils/models';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-generator',
@@ -10,10 +11,26 @@ export class FormGeneratorComponent implements OnInit {
 
   @Input()
   formConfig: FieldConfig[];
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  ngOnChanges({formConfig: { currentValue }}: SimpleChanges) {
+    if (currentValue) {
+      this.createForm(currentValue);
+    }
+  }
+
+  createForm(formConfig: FieldConfig[]) {
+    formConfig
+      .filter(it => !['button', 'submit'].includes(it.type))
+      .forEach(fieldConfig => {
+        this.form.addControl(fieldConfig.name, this.fb.control(fieldConfig.value));
+      });
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({});
   }
 
 }
