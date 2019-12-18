35da2fdb6dc425c456e4d992853f36c9d14ec430
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 09:51:47 +0100

    apka / ngrx - added ngrx feature cart

--- apka/src/app/modules/cart/cart.module.ts
+++ apka/src/app/modules/cart/cart.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import * as fromCart from './store/cart.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { CartEffects } from './store/cart.effects';
 
 @NgModule({
   declarations: [CartComponent],
@@ -10,6 +14,8 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       {path: '', component: CartComponent}
     ]),
+    StoreModule.forFeature(fromCart.cartFeatureKey, fromCart.reducer),
+    EffectsModule.forFeature([CartEffects]),
   ]
 })
 export class CartModule { }
---
+++ apka/src/app/modules/cart/store/cart.reducer.ts
@@ -0,0 +1,25 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as CartActions from './cart.actions';
+
+export const cartFeatureKey = 'cart';
+
+export interface State {
+
+}
+
+export const initialState: State = {
+
+};
+
+const cartReducer = createReducer(
+  initialState,
+
+  on(CartActions.loadCarts, state => state),
+  on(CartActions.loadCartsSuccess, (state, action) => state),
+  on(CartActions.loadCartsFailure, (state, action) => state),
+
+);
+
+export function reducer(state: State | undefined, action: Action) {
+  return cartReducer(state, action);
+}
