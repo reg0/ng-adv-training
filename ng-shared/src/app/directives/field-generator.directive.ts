d746f946f52bda084e1f0a15153759ed5145a20d
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2019-12-18 14:52:25 +0100

    forms - usage of the directive

--- ng-shared/src/app/directives/field-generator.directive.ts
+++ ng-shared/src/app/directives/field-generator.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 
 @Directive({
   selector: '[appFieldGenerator]'
 })
 export class FieldGeneratorDirective {
-
   constructor() { }
+
+  @Input() set appFieldGenerator(config) {
+    debugger;
+  }
+  
 
 }
